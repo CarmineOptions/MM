434a4caaeaa0d432981a14afdb5fc0d006f673d2
Chepelau <chepelau.a@gmail.com>
2025-05-28 23:05:21 +0200

    Add order classes

--- MM/marketmaking/order.py
+++ MM/marketmaking/order.py
@@ -1,3 +1,44 @@
+from dataclasses import dataclass
+from decimal import Decimal
+from typing import Literal
 
-class Order:
-    pass
+
+@dataclass(frozen=True)
+class BasicOrder:
+    """
+    Simple class representing on-chain order. 
+    All values are expected to be in "raw" form.
+    """
+    price: Decimal
+    amount: Decimal
+    amount_remaining: Decimal
+
+    order_id: int
+    market_id: int
+
+    order_side: str
+    entry_time: int
+
+
+    @staticmethod
+    def from_remus_order(o: dict) -> "BasicOrder":
+        return BasicOrder(
+            price = Decimal(o['price']),
+            amount = Decimal(o['amount']),
+            amount_remaining = Decimal(o['amount_remaining']),
+            order_id = int(o['maker_order_id']),
+            market_id= int(o['market_id']),
+            order_side = o['order_side'].variant,
+            entry_time = int(o['entry_time'])
+        )
+
+
+@dataclass
+class FutureOrder:
+    """
+    Class representing an order that will be sent to the chain.
+    """
+    order_side: str # TODO: use Literal or enum here
+    amount: int
+    price: int
+
