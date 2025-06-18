e82bb324e77f0e3bc63c909faf9ea2fe29994e79
Chepelau <chepelau.a@gmail.com>
2025-06-18 15:32:45 +0200

    Remove commented code

--- MM/markets/market.py
+++ MM/markets/market.py
@@ -7,16 +7,6 @@ from starknet_py.net.client_models import Calls
 from marketmaking.waccount import WAccount
 
 
-# @dataclass
-# class UnsettledAmounts:
-#     '''
-#     Represents the unsettled amounts of base and quote tokens in a market.
-#     This is used to track the amounts that are for example matched but not yet settled,
-#     and need to be somehow withdrawn or claimed.
-#     '''
-#     base: InstrumentAmount
-#     quote: InstrumentAmount
-
 class Market(ABC):
 
     @abstractmethod
@@ -39,9 +29,3 @@ class Market(ABC):
     async def get_total_position(self) -> PositionInfo:
         pass
 
-    # @abstractmethod
-    # def get_unsettled_amounts(self, which: Literal["base", "quote", "both"]) -> UnsettledAmounts:
-    #     """
-    #     Returns the unsettled amounts of base and quote tokens.
-    #     """
-    #     raise NotImplementedError
