3217b64ce4a90046b15f6d39655dfe802ea71be8
Chepelau <chepelau.a@gmail.com>
2025-08-11 12:50:41 +0200

    Remove BoundedReconciler

--- MM/marketmaking/reconciling/__init__.py
+++ MM/marketmaking/reconciling/__init__.py
@@ -1,6 +1,5 @@
 from decimal import Decimal
 from .order_reconciler import OrderReconciler
-from .bounded_reconciler import BoundedReconciler
 from .always_replace_reconciler import AlwaysReplaceOrderReconciler
 from .tolerance_reconciler import ToleranceOrderReconciler
 
@@ -8,17 +7,6 @@ from cfg.cfg_classes import ReconcilerConfig
 
 
 def get_reconciler(cfg: ReconcilerConfig) -> OrderReconciler:
-    if cfg.name == "bounded_reconciler":
-        return BoundedReconciler(
-            max_relative_distance_from_fp=Decimal(
-                cfg.args["max_relative_distance_from_fp"]
-            ),
-            min_relative_distnace_from_fp=Decimal(
-                cfg.args["min_relative_distance_from_fp"]
-            ),
-            minimal_remaining_size=Decimal(cfg.args["minimal_remaining_size"]),
-            max_orders_per_side=int(cfg.args["max_orders_per_side"]),
-        )
     if cfg.name == "always_replace_reconciler":
         return AlwaysReplaceOrderReconciler()
     
